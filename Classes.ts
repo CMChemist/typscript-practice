0c494eb402f117d7b364ee60d60030c2922b491c
github username <josephtrettevik@gmail.com>
2020-05-17 22:40:39 -0700

    practice declaring classes

---
+++ Classes.ts
@@ -0,0 +1,12 @@
+class Car { 
+
+    engine:string; 
+
+    constructor(engine:string) { 
+       this.engine = engine 
+    }  
+ 
+    disp():void { 
+       console.log("Engine is  :   "+this.engine) 
+    } 
+ }
